c8bc683c0ea8b49e14f8e35abae3c4f417a5ce47
jasonkiptoo <kiptoojason@gmail.com>
2022-08-07 13:50:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+main.remove();
+
+
+
+
+//create new header <h1> 
+
+const newHeader=document.createElement(`h1`);
+//Header with id node with  Victory
+newHeader.id="victory"
+//text content with name
+newHeader.textContent="Jayson is the champion"
+   
+
